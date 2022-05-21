0508eb91c600782ac7e4845ca844f5ed2901b276
Reginald Amedee <reginald.amedee@gmail.com>
2022-05-21 18:59:24 -0400

    added error handling for route that doesnt exist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/Navbar';
@@ -10,61 +10,63 @@ import NewProducts from './components/NewProducts';
 import User from './components/User';
 import UserDetail from './components/UserDetails';
 import Admin from './components/Admin';
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import './App.css';
 
 function App() {
 
   const [specificUser, setSpecificUser] = useState({})
+  const [userFilled, setUserFilled] = useState(false)
+  
+
+    
+    const filterUserId = (id) => {
+        const newItem = user.filter((item) => {
+            return item.id === id
+        })
+        setUserFilled(true)
+        setSpecificUser(newItem)
+        
+        
+        
+    }
 
 
-
-  const filterUserId = (id) => {
-    const newItem = user.filter((item) => {
-      return item.id === id
-    })
-    setSpecificUser(newItem)
-
-
-
-  }
-
-
-  const user = [{
-    name: 'Starscourge Radahn',
-    email: 'redlion@caelid.com',
-    userName: 'LeanordChampion',
-    id: Math.floor(Math.random() * 1000000)
-  },
-  {
-    name: 'Godrick the Grafted',
-    email: 'stormveilking@limgrave.com',
-    userName: 'Godfreys Fave',
-    id: Math.floor(Math.random() * 1000000)
-  }, {
-    name: 'Malenia the severed',
-    email: 'oneArmValkyrie@haligtree.net',
-    userName: 'Blade of Miquella',
-    id: Math.floor(Math.random() * 1000000)
-  }, {
-    name: 'Queen Renalla',
-    email: 'iheartradagon@rayalucaria.net',
-    userName: 'Ranni Mom',
-    id: Math.floor(Math.random() * 1000000)
-  },
+    const user = [{
+        name: 'Starscourge Radahn',
+        email: 'redlion@caelid.com',
+        userName: 'LeanordChampion',
+        id: Math.floor(Math.random() * 1000000)
+    },
+     {
+        name: 'Godrick the Grafted',
+        email: 'stormveilking@limgrave.com',
+        userName: 'Godfreys Fave',
+        id: Math.floor(Math.random() * 1000000)
+    }, {
+        name: 'Malenia the severed',
+        email: 'oneArmValkyrie@haligtree.net',
+        userName: 'Blade of Miquella',
+        id: Math.floor(Math.random() * 1000000)
+    }, {
+        name: 'Queen Renalla',
+        email: 'iheartradagon@rayalucaria.net',
+        userName: 'Ranni Mom',
+        id: Math.floor(Math.random() * 1000000)
+    },
   {
     name: "King Radagon",
     email: 'goldenorderboy@leyndell.com',
     userName: 'red hair Rada',
     id: Math.floor(Math.random() * 1000000)
   },
-  {
-    name: 'Mohg, Lord of Blood',
-    email: 'miquellafosterdad@bloodpalace.com',
-    userName: 'you have nihil chance!',
-    id: Math.floor(Math.random() * 1000000)
-  }]
+{
+  name: 'Mohg, Lord of Blood',
+  email: 'miquellafosterdad@bloodpalace.com',
+  userName: 'you have nihil chance!',
+  id: Math.floor(Math.random() * 1000000)
+}]
 
 
 
@@ -81,10 +83,10 @@ function App() {
           <Route path="featured" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
         </Route>
-        <Route path="users" element={<User user={user} filterUserId={filterUserId} />}>
+        <Route path="users" element={<User user={user} filterUserId={filterUserId}/>}>
           <Route path="admin" element={<Admin />} />
         </Route>
-        <Route path="users/:userId" element={<UserDetail specificUser={specificUser} />} />
+        <Route path="users/:userId" element={<UserDetail specificUser={specificUser} userFilled={userFilled}/>} />
       </Routes>
     </>
   );
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -5,32 +5,39 @@ import { useParams, useNavigate } from "react-router-dom";
 const UserDetail = (props) => {
 
     const navigate = useNavigate();
-    const {specificUser} = props;
+    const { specificUser, userFilled } = props;
     const { userId } = useParams();
 
 
-    
 
-    
+
+
 
 
     return <>
-    <div className="user-detail">
+        <div className="user-detail">
+            {!userFilled ?
+                <>
+                    <p>User Not Found</p>
+                    <button onClick={() => { navigate(-1) }}> Go Back</button>
+                </>
+                :
+                specificUser.map((item) => {
 
-    
-    {specificUser.map((item) => {
-        
-        return <div>
-            <p>User Name: {item.userName}</p>
-            <p> Name: {item.name}</p>
-            <p>Email: {item.email}</p>
+                    return <>
+
+                        <div>
+                            <p>User Name: {item.userName}</p>
+                            <p>Name: {item.name}</p>
+                            <p>Email: {item.email}</p>
+                        </div>
+                        <button onClick={() => { navigate(-1) }}> Go Back</button>
+                    </>
+                })
+            }
         </div>
-        
-    })}
-    <button onClick={() => {navigate(-1)}}>Go Back</button>
-    
-    </div>
     </>
 }
 
 export default UserDetail
+
